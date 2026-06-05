418cb4bc5f3531a3cf8192d95d1af4805f417897
lbercovitch <leah.bercovitch@gmail.com>
2026-06-04 23:42:33 -0700

    fixing header css

--- colour-conjurer/src/components/Header.tsx
+++ colour-conjurer/src/components/Header.tsx
@@ -1,13 +1,15 @@
 function Header() {
   return (
-    <div className="h-75 sm:h-100 bg-slate-950">
-      <div className="relative">
-        <h1 className="z-20 absolute max-w-50 md:max-w-100 top-40 md:top-30 lg:top-25 xl:top-15 left-10 md:left-15 lg:left-20 xl:left-25 text-4xl sm:text-6xl md:text-7xl lg:text-8xl xl:text-9xl text-neutral-50 text-shadow-amber-h1 font-slackey ">
+    <div className="h-100 bg-slate-950 flex items-center justify-center relative overflow-hidden">
+      <div className="relative sm:static flex flex-row items-center justify-center gap-8 text-center sm:text-left w-full h-full px-4">
+        <h1 className="z-20 absolute sm:static bottom-15 max-w-50 md:max-w-80 lg:max-w-100 xl:max-w-150
+          text-4xl sm:text-6xl md:text-7xl lg:text-8xl xl:text-9xl text-neutral-50 text-shadow-amber-h1 font-slackey"
+        >
           Colour Conjuror
         </h1>
         <img
           src="./colour-conjuror-cat.png"
-          className="z-10 h-75 sm:h-100 absolute sm:right-10 md:left-95 lg:left-150 xl:left-180"
+          className="absolute sm:static top-5 z-10 h-64 sm:h-80 md:h-100 object-contain"
         />
       </div>
     </div>
