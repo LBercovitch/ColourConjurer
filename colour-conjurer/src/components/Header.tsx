68a6d34e57bac5b7611a662b8818259d50827dfa
lbercovitch <leah.bercovitch@gmail.com>
2026-06-04 23:49:21 -0700

    adding bg colour

--- colour-conjurer/src/components/Header.tsx
+++ colour-conjurer/src/components/Header.tsx
@@ -1,6 +1,6 @@
 function Header() {
   return (
-    <div className="h-100 bg-slate-950 flex items-center justify-center relative overflow-hidden">
+    <div className="h-100 flex items-center justify-center relative overflow-hidden">
       <div className="relative sm:static flex flex-row items-center justify-center gap-8 text-center sm:text-left w-full h-full px-4">
         <h1 className="z-20 absolute sm:static bottom-15 max-w-50 md:max-w-80 lg:max-w-100 xl:max-w-150
           text-4xl sm:text-6xl md:text-7xl lg:text-8xl xl:text-9xl text-neutral-50 text-shadow-amber-h1 font-slackey"
